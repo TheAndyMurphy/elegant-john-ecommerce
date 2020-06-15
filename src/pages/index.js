330f8bf1901e06491252f2cc5ad741ebc0797ada
TheAndyMurphy <andrewmurphydublin@gmail.com>
2020-06-15 15:21:06 +0100

    Updated apis

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,11 +37,6 @@ render={data => (
           <div className="Catalogue__item" key={product.id}>
             <div
               className="Product snipcart-add-item"
-              data-item-id={product.id}
-              data-item-price={product.price}
-              data-item-image={product.image.url}
-              data-item-name={product.name}
-              data-item-url={`/`}
             >
               <div className="Product__image">
                 <Img sizes={product.image.sizes} />
@@ -52,7 +47,16 @@ render={data => (
                     {product.price}€
                   </div>
                 </div>
-                <span className="Product__buy">Buy now</span>
+                <button 
+                  className="Product__buy Product snipcart-add-item"
+                  data-item-id={product.id}
+                  data-item-price={product.price}
+                  data-item-image={product.image.url}
+                  data-item-name={product.name}
+                  data-item-url={`/`}
+                >
+                  Buy now
+                </button>
               </div>
             </div>
           </div>
